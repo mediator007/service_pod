fe894bf3563e51cb94aaf907eca8908fae6f8043
mediator007 <mediator007@mail.ru>
2023-04-11 12:31:14 +0300

    Fix user table

--- models/models.py
+++ models/models.py
@@ -5,8 +5,14 @@ from db.db import Base
 
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "app_user"
     id = Column(Integer, primary_key=True)
     name = Column(String(100), unique=False, nullable=False)
     email = Column(String(100), unique=True, nullable=False)
     created_at = Column(DateTime, index=True, default=datetime.utcnow)
+
+
+class Entity(Base):
+    __tablename__ = "entity"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=False, nullable=False)
